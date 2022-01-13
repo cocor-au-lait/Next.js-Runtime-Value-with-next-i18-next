147e0c4ddc5458747db69526001bdd06af23e7af
Jun Koyama <cocor.au.lait@gmail.com>
2022-01-14 02:57:46 +0900

    Phase 5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials"],
   framework: "@storybook/react",
   core: {
     builder: "storybook-builder-vite",
