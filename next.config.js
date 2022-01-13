068fbfaafc0f13df6f1dc72eee7880c4831ed1ec
Jun Koyama <cocor.au.lait@gmail.com>
2022-01-13 21:25:50 +0900

    Phase 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    HOGE: process.env.HOGE,
+  },
+  publicRuntimeConfig: {
+    HOGE: process.env.HOGE
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppContext, AppInitialProps, AppProps } from "next/app";
+import App from "next/app";
+import getConfig from "next/config";
+import { NextComponentType } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props {
+  config: any;
 }
 
-export default MyApp
+const MyApp: NextComponentType<
+  AppContext,
+  AppInitialProps & Props,
+  AppProps & Props
+> = ({ Component, pageProps, config }) => {
+  console.log(config);
+  return <Component {...pageProps} />;
+};
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  const config = getConfig();
+  return { ...appProps, config };
+};
+
+export default MyApp;
